612c49102f8bec61816667b3e6aa1308e0ef2a2e
vagrant <vagrant@homestead>
2016-12-01 17:05:34 +0000

    Change mood to mode in glossary

--- resources/views/glossary/index.blade.php
+++ resources/views/glossary/index.blade.php
@@ -6,7 +6,7 @@
 	<li><a href = '/glossary/groups'>Groups</a></li>
 	<li><a href = '/glossary/classes'>Classes</a></li>
 	<li><a href = '/glossary/glosses'>Glosses</a></li>
-	<li><a href = '/glossary/moods'>Moods</a></li>
+	<li><a href = '/glossary/modes'>Modes</a></li>
 	<li><a href = '/glossary/orders'>Orders</a></li>
 	<li><a href = '/glossary/tenses'>Tenses</a></li>
 	<li><a href = '/glossary/slots'>Slots</a></li>

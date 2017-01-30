318639351898fb4706a11fc1b1a1a17c753ac35e
vagrant <vagrant@homestead>
2017-01-30 06:29:51 +0000

    fix model component error

--- resources/views/components/model.blade.php
+++ resources/views/components/model.blade.php
@@ -3,11 +3,11 @@
 	@if(isset($header))
 		<header class="card-header">
 			<p class="card-header-title is-4" style="font-size: 20px;">
-				@if($smallCaps)
+				@if(isset($smallCaps))
 				<span class="gloss">
 				@endif
 				{{ $header }}
-				@if($smallCaps)
+				@if(isset($smallCaps))
 				</span>
 				@endif
 			</p>

1bffbbead55645100ec8a032be3c2aa48f2e7c2a
Graham Still <gstill92@gmail.com>
2017-03-21 01:20:10 +0000

    Fix example issue

--- resources/views/components/model.blade.php
+++ resources/views/components/model.blade.php
@@ -42,7 +42,7 @@
 				<?php $last = $history->last(); ?>
 				@if($last && $last->key != 'created_at')
 					@component('components.model.field', ['width' => 'is-12', 'label' => 'Last Edit'])
-							{{ $last->userResponsible() ? $last->userResponsible()->name : 'Anonymous' }} changed the {{ $last->fieldName() }} field from "{{ $last->oldValue() }}" to "{{ $last->newValue() }}" at {{ $last->updated_at }}
+							{{ $last->userResponsible() ? $last->userResponsible()->name : 'Anonymous' }} changed the {{ $last->fieldName() }} field from "{{ $last->old_value }}" at {{ $last->updated_at }}
 					@endcomponent
 				@endif
 			</model-tab>
--- resources/views/examples/show.blade.php
+++ resources/views/examples/show.blade.php
@@ -47,7 +47,4 @@
 		</div>
 	</model-tab>
 @endcomponent
-
-
-
 @endsection

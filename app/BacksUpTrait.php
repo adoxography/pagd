66aa037832da0ba2ee1a8fa515aef66c4ef9b86c
Graham Still <gstill92@gmail.com>
2017-03-23 22:21:06 +0000

    Move backup into a trait

--- app/BacksUpTrait.php
+++ app/BacksUpTrait.php
@@ -2,9 +2,16 @@
 
 namespace App;
 
+use App\Events\Backup;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
+
 trait BacksUpTrait {
-	
 	public static function bootBacksUpTrait() {
-		
+		static::saved(function($model) {
+			event(new Backup());
+		});
 	}
 }

e039a6d4d6ab58a6199f02875a05033b5622f87f
Graham Still <gstill92@gmail.com>
2017-06-07 23:03:08 +0000

    Update header links

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -13,9 +13,9 @@
 					<div class="tile is-child" id="main-menu">
 						<ul>
 							<li><h5 class="subtitle is-5"><a href="/variables">Structural survey</a></h5></li>
-							<li><h5 class="subtitle is-5"><a href="#">Verb forms</a></h5></li>
-							<li><h5 class="subtitle is-5"><a href="#"><nobr>Nominal forms</nobr></a></h5></li>
-							<li><h5 class="subtitle is-5"><a href="#">Phonology</a></h5></li>
+							<li><h5 class="subtitle is-5"><a href="/verb%20forms">Verb forms</a></h5></li>
+							<li><h5 class="subtitle is-5"><a href="/nominal%20forms"><nobr>Nominal forms</nobr></a></h5></li>
+							<li><h5 class="subtitle is-5"><a href="/phonology">Phonology</a></h5></li>
 							<li><h5 class="subtitle is-5"><a href="/sources">Bibliography</a></h5></li>
 						</ul>
 					</div>

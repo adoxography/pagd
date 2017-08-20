fd45a25f3f5901409f96397a45aac96b684dc249
vagrant <vagrant@homestead>
2017-08-20 02:30:37 +0000

    Only include urgent tickets in summary if there are urgent tickets to begin with

--- resources/views/tickets/mail/summary.blade.php
+++ resources/views/tickets/mail/summary.blade.php
@@ -1,13 +1,15 @@
 @component('mail::message')
 Hi {{ $user->firstName }},
 
+@if(count($urgentTickets) > 0)
 The following urgent tickets remain open:
 
 @foreach ($urgentTickets as $ticket)
 - [{{ $ticket->title }}](http://alglang.net/tickets/{{ $ticket->id }})
 @endforeach
+@endif
 
-Additionally, the following non-urgent tickets were opened in the last day:
+The following non-urgent tickets were opened in the last day:
 
 @if (count($nonUrgentTickets) > 0)
 @foreach ($nonUrgentTickets as $ticket)

590d4f7195160dbd73aeab7fb2e8cc33ce9aeb4a
Adoxography <nevermore667@gmail.com>
2018-03-12 21:13:39 -0500

    Fix layout of general search results

--- resources/views/search/general-results.blade.php
+++ resources/views/search/general-results.blade.php
@@ -1,20 +1,17 @@
 @extends('layout', ['title' => 'Search results'])
 
-@section('content')
-	<div class="heading">
-		<h1 class="title">Search Results</h1>
-	</div>
-	<br />
+@section('title')
+	Search results
+@endsection
 
-	<div class="box">
-		@if(count($results) > 0)
-			<ul>
-				@foreach($results as $result)
-					<li><a href="/{{ $table }}/{{ $result['id'] }}">{{ $result['display'] }}</a></li>
-				@endforeach
-			</ul>
-		@else
-			No results found
-		@endif
-	</div>
+@section('content')
+	@if(count($results) > 0)
+		<ul>
+			@foreach($results as $result)
+				<li><a href="/{{ $table }}/{{ $result['id'] }}">{{ $result['display'] }}</a></li>
+			@endforeach
+		</ul>
+	@else
+		No results found
+	@endif
 @endsection

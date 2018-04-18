394968975a99d709b678a1a18bd5bce75ec7b824
Adoxography <gstill@uw.edu>
2018-04-17 23:36:55 -0700

    Edit the right file

--- packages/algling/verbals/src/Models/Structure.php
+++ packages/algling/verbals/src/Models/Structure.php
@@ -175,7 +175,9 @@ class Structure extends Model
     public function assignSubclass()
     {
         if ($this->formClass->name == 'AI+O') {
-            if ($this->secondaryObject->person == '3') {
+            $object = $this->primaryObject ?: $this->secondaryObject;
+
+            if ($object->person == '3') {
                 $subclass = 'AI+O (+3)';
             } else {
                 $subclass = 'AI+O (+0)';

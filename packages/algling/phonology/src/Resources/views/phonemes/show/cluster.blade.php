e7b971066adf452bec9088d47b50be268381b39d
Adoxography <gstill@uw.edu>
2018-09-30 21:30:53 -0500

    Remove old phonology directory

--- packages/algling/phonology/src/Resources/views/phonemes/show/cluster.blade.php
+++
@@ -1,9 +0,0 @@
-<span class="is-one-line">
-	<span class="label">First segment:</span>
-	{!! $phoneme->features->firstSegment->present('link') !!}
-</span>
-
-<span class="is-one-line">
-	<span class="label">Second segment:</span>
-	{!! $phoneme->features->secondSegment->present('link') !!}
-</span>
--- packages/algling/phonology/src/Resources/views/reflexes/create.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout', ['title' => 'Add reflex'])
-
-@section('title')
-    Add reflex
-@endsection
-
-@section('content')
-	@include('phon::reflexes.partials.form', ['method' => 'POST', 'action' => '/reflexes'])
-@endsection

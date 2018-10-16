70f025bcc7e2fab32c22a3fdc990b1c0b108037d
Adoxography <gstill@uw.edu>
2018-10-16 17:04:29 -0500

    Add HasMorphemesInterface to morphemes that use the HasMorphemes trait

---
+++ app/Interfaces/Morphemes/HasMorphemesInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+interface HasMorphemesInterface
+{
+    public function morphemes();
+
+    public function connectMorphemes();
+
+    public function getMorphType();
+
+    public function morphemeSequence();
+
+    public function morphemeList();
+
+    public function printMorphemes();
+
+    public function containsMorphemes();
+
+    public function morphemesToJson();
+}

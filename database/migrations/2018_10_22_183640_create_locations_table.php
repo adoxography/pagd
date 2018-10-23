a1cc324059d65d477c53cf7c549d2fc0ab785662
Adoxography <gstill@uw.edu>
2018-10-22 19:57:09 -0500

    Add location migrations

---
+++ database/migrations/2018_10_22_183640_create_locations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('type');
+            $table->text('position');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Locations');
+    }
+}

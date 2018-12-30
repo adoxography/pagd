9c8e35560d7af31c77800ffc80cf5a42aeeb64ce
Adoxography <gstill@uw.edu>
2018-12-30 01:43:18 -0600

    Update registration UI & UX

--- public/assets/merchant/js/chunk10.js
+++ public/assets/merchant/js/chunk10.js
@@ -6,18 +6,47 @@ webpackJsonp([10,27],{
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__mixins_OldErrors__ = __webpack_require__("./resources/assets/js/mixins/OldErrors.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__mixins_OldSources__ = __webpack_require__("./resources/assets/js/mixins/OldSources.js");
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 
 
 /* harmony default export */ __webpack_exports__["a"] = ({
-    mixins: [__WEBPACK_IMPORTED_MODULE_0__mixins_OldErrors__["a" /* default */], __WEBPACK_IMPORTED_MODULE_1__mixins_OldSources__["a" /* default */]],
+  mixins: [__WEBPACK_IMPORTED_MODULE_0__mixins_OldErrors__["a" /* default */], __WEBPACK_IMPORTED_MODULE_1__mixins_OldSources__["a" /* default */]],
 
-    data: function data() {
-        return {
-            sources: []
-        };
+  props: ['method', 'action'],
+
+  data: function data() {
+    return {
+      sources: [],
+      csrfToken: Laravel.csrfToken
+    };
+  },
+
+
+  methods: {
+    validateBeforeSubmit: function validateBeforeSubmit(event) {
+      var _this = this;
+
+      this.$validator.validateAll().then(function (result) {
+        if (result) {
+          _this.$refs.form.submit();
+        }
+      });
     }
+  }
 });
 
 /***/ }),
@@ -42,6 +71,57 @@ webpackJsonp([10,27],{
 		};
 	}
 });
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/assets/js/components/forms/Form.vue?vue&type=template&id=68350a1b&":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "form",
+    {
+      ref: "form",
+      attrs: { method: _vm.method, action: _vm.action },
+      on: {
+        submit: function($event) {
+          $event.preventDefault()
+          _vm.validateBeforeSubmit($event)
+        }
+      }
+    },
+    [
+      _c("input", {
+        attrs: { name: "_token", type: "hidden" },
+        domProps: { value: _vm.csrfToken }
+      }),
+      _vm._v(" "),
+      _vm._t("default"),
+      _vm._v(" "),
+      _c("div", { staticClass: "field" }, [
+        _c(
+          "button",
+          {
+            staticClass: "button is-primary",
+            attrs: { type: "submit", disabled: _vm.errors.any() }
+          },
+          [_vm._v("Submit")]
+        )
+      ])
+    ],
+    2
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
 
 /***/ }),
 
@@ -88,20 +168,21 @@ var Datalist = function () {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__Form_vue_vue_type_script_lang_js___ = __webpack_require__("./resources/assets/js/components/forms/Form.vue?vue&type=script&lang=js&");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__Form_vue_vue_type_template_id_68350a1b___ = __webpack_require__("./resources/assets/js/components/forms/Form.vue?vue&type=template&id=68350a1b&");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__Form_vue_vue_type_script_lang_js___ = __webpack_require__("./resources/assets/js/components/forms/Form.vue?vue&type=script&lang=js&");
 /* empty harmony namespace reexport */
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_runtime_componentNormalizer_js__ = __webpack_require__("./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
-var render, staticRenderFns
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__node_modules_vue_loader_lib_runtime_componentNormalizer_js__ = __webpack_require__("./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
 
 
 
 
 /* normalize component */
 
-var component = Object(__WEBPACK_IMPORTED_MODULE_1__node_modules_vue_loader_lib_runtime_componentNormalizer_js__["a" /* default */])(
-  __WEBPACK_IMPORTED_MODULE_0__Form_vue_vue_type_script_lang_js___["a" /* default */],
-  render,
-  staticRenderFns,
+var component = Object(__WEBPACK_IMPORTED_MODULE_2__node_modules_vue_loader_lib_runtime_componentNormalizer_js__["a" /* default */])(
+  __WEBPACK_IMPORTED_MODULE_1__Form_vue_vue_type_script_lang_js___["a" /* default */],
+  __WEBPACK_IMPORTED_MODULE_0__Form_vue_vue_type_template_id_68350a1b___["a" /* render */],
+  __WEBPACK_IMPORTED_MODULE_0__Form_vue_vue_type_template_id_68350a1b___["b" /* staticRenderFns */],
   false,
   null,
   null,
@@ -120,7 +201,12 @@ if (false) {
     } else {
       api.reload('68350a1b', component.options)
     }
-    
+    module.hot.accept("./Form.vue?vue&type=template&id=68350a1b&", function () {
+      api.rerender('68350a1b', {
+        render: render,
+        staticRenderFns: staticRenderFns
+      })
+    })
   }
 }
 component.options.__file = "resources/assets/js/components/forms/Form.vue"
@@ -135,6 +221,17 @@ component.options.__file = "resources/assets/js/components/forms/Form.vue"
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Form_vue_vue_type_script_lang_js___ = __webpack_require__("./node_modules/babel-loader/lib/index.js??ref--4-0!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/assets/js/components/forms/Form.vue?vue&type=script&lang=js&");
 /* unused harmony namespace reexport */
  /* harmony default export */ __webpack_exports__["a"] = (__WEBPACK_IMPORTED_MODULE_0__node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Form_vue_vue_type_script_lang_js___["a" /* default */]); 
+
+/***/ }),
+
+/***/ "./resources/assets/js/components/forms/Form.vue?vue&type=template&id=68350a1b&":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Form_vue_vue_type_template_id_68350a1b___ = __webpack_require__("./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/assets/js/components/forms/Form.vue?vue&type=template&id=68350a1b&");
+/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "a", function() { return __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Form_vue_vue_type_template_id_68350a1b___["a"]; });
+/* harmony namespace reexport (by used) */ __webpack_require__.d(__webpack_exports__, "b", function() { return __WEBPACK_IMPORTED_MODULE_0__node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_Form_vue_vue_type_template_id_68350a1b___["b"]; });
+
 
 /***/ }),
 
@@ -198,34 +295,19 @@ component.options.__file = "resources/assets/js/components/forms/Paradigm.vue"
 
 "use strict";
 /* harmony default export */ __webpack_exports__["a"] = ({
-    props: ['oldErrors'],
+  props: ['oldErrors'],
 
-    mounted: function mounted() {
-        var _this = this;
+  mounted: function mounted() {
+    var _this = this;
 
-        if (this.oldErrors) {
-            Vue.nextTick(function () {
-                _.forEach(_this.oldErrors, function (errors, field) {
-                    field = field.split('_')[0];
-
-                    errors.forEach(function (message) {
-                        _this.errors.add(field, message, 'database');
-                    });
-                });
-            });
-        }
-    },
-
-
-    methods: {
-        validateBeforeSubmit: function validateBeforeSubmit(event) {
-            this.$validator.validateAll().then(function (result) {
-                if (result) {
-                    return;
-                }
-            });
-        }
-    }
+    if (this.oldErrors) {
+      _.forEach(this.oldErrors, function (errors, field) {
+        errors.forEach(function (message) {
+          return _this.$root.errors.add({ field: field, msg: message });
+        });
+      });
+    };
+  }
 });
 
 /***/ }),
--- resources/assets/js/mixins/OldErrors.js
+++ resources/assets/js/mixins/OldErrors.js
@@ -3,25 +3,9 @@ export default {
 
 	mounted() {
 		if(this.oldErrors) {
-		    Vue.nextTick(() => {
-                _.forEach(this.oldErrors, (errors, field) => {
-                    field = field.split('_')[0];
-
-                    errors.forEach(message => {
-                        this.errors.add(field, message, 'database');
-                    })
-                });
-            });
-        }
-	},
-
-	methods: {
-		validateBeforeSubmit(event) {
-      this.$validator.validateAll().then(result => {
-        if (result) {
-          return;
-        }
+      _.forEach(this.oldErrors, (errors, field) => {
+        errors.forEach(message => this.$root.errors.add({field: field, msg: message}));
       });
-		}
+    };
 	}
 }

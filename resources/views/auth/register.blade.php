a7b11be7c10db33d6e1c2df351f755537f0cdba2
Adoxography <gstill@uw.edu>
2018-12-30 13:50:48 -0600

    Clean up register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,10 @@
 @extends('layout')
 
 @section('title')
-    Register
+<h1 class="title is-4">Register</h1>
 @endsection
 
 @section('content')
-@component('components.form', ['url' => url('/register')])
 <alg-form method="POST" action="{{ url('/register') }}" :old-errors="{{ $errors }}">
     <table class="table details">
         <tbody>
@@ -111,64 +110,3 @@
     </table>
 </alg-form>
 @endsection
-    
-
-
-
-
-
-        {{--<div class="columns is-multiline">--}}
-            {{--<div class="column is-half">--}}
-                {{--<div class="columns">--}}
-                    {{--<div class="column">--}}
-                        {{--@component('components.form.text', ['name' => 'first_name', 'label' => 'First name', 'required' => 'true'])--}}
-                            {{--@slot('value')--}}
-                                {{--{{ old('first_name') }}--}}
-                            {{--@endslot--}}
-                        {{--@endcomponent--}}
-                    {{--</div>--}}
-                    {{--<div class="column">--}}
-                        {{--@component('components.form.text', ['name' => 'last_name', 'label' => 'Last name', 'required' => 'true'])--}}
-                            {{--@slot('value')--}}
-                                {{--{{ old('last_name') }}--}}
-                            {{--@endslot--}}
-                        {{--@endcomponent--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--<div class="column is-half">   --}}
-                {{--@component('components.form.text', ['name' => 'email', 'type' => 'email', 'label' => 'Email', 'required' => 'true'])--}}
-                    {{--@slot('value')--}}
-                        {{--{{ old('email') }}--}}
-                    {{--@endslot--}}
-                {{--@endcomponent--}}
-            {{--</div>--}}
-
-            {{--<div class="column is-one-quarter">--}}
-                {{--@component('components.form.text', ['name' => 'password', 'type' => 'password', 'label' => 'Password', 'required' => 'true'])--}}
-                    {{--@slot('value')--}}
-                        {{--{{ old('password') }}--}}
-                    {{--@endslot--}}
-                {{--@endcomponent--}}
-            {{--</div>--}}
-            {{--<div class="column is-one-quarter">--}}
-                {{--@component('components.form.text', ['name' => 'password_confirmation', 'type' => 'password', 'label' => 'Confirm Password', 'required' => 'true'])--}}
-                    {{--@slot('value')--}}
-                        {{--{{ old('password_confirmation') }}--}}
-                    {{--@endslot--}}
-                {{--@endcomponent--}}
-            {{--</div>--}}
-            {{--<div class="column is-half">--}}
-                {{--@component('components.form.text', ['name' => 'verification', 'label' => 'Verification Code', 'required' => 'true'])--}}
-                    {{--@slot('value')--}}
-                        {{--{{ old('verification') }}--}}
-                    {{--@endslot--}}
-                {{--@endcomponent--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        {{--<div class="field">--}}
-            {{--<button type="submit" class="button is-primary">Register</button>--}}
-        {{--</div>--}}
-    {{--@endcomponent--}}
-    {{--@include('errors.list')--}}
-{{--@endsection--}}

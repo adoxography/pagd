cfc2c5c2f9cf3fc9148318edd8b9f15eb1be8114
Adoxography <gstill@uw.edu>
2019-01-10 22:35:02 -0600

    Update source form to new layout

--- resources/views/sources/edit.blade.php
+++ resources/views/sources/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layout', ['title' => "Edit {$source->display}"])
 
 @section('title')
-	Editing {{ $source->display }}
+    <h1 class="title is-4">Editing {!! $source->present() !!}</h1>
 @endsection
 
 @section('content')

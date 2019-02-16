a50b79deb41cba1435a84f541f46aa16763e4af3
Adoxography <gstill@uw.edu>
2019-02-15 19:49:28 -0600

    Update examples' basic details page

--- resources/views/words/examples/show/basic.blade.php
+++ resources/views/words/examples/show/basic.blade.php
@@ -1,45 +1,76 @@
 @extends('words.examples.show')
 
 @section('details')
-	<div class="columns">
-		<div class="column">
-			@if ($example->form)
-				<div class="field">
-					<span class="label">Form</span>
-					{!! $example->form->present('stub') !!}
-				</div>
-			@endif
-			<div class="field">
-				<span class="label">Morphology</span>
-				{!! $example->present('phonemicForm') !!}
-				{!! $example->present('morphemes') !!}
-			</div>
-			<div class="field">
-				<span class="label">Translation</span>
-				{{ $example->translation }}
-			</div>
-		</div>
-		<div class="column">
-			@if($example->parent)
-				<div class="field">
-					<span class="label">Historical notes</span>
-					Parent: {!! $example->parent->present()->as('unique', 'link')->then('language')->as('link') !!}
-				</div>
-			@endif
-			@if($example->publicNotes)
-				<div class="field">
-					<span class="label">Notes</span>
-					{!! replaceTags($example->publicNotes, $example->language->id) !!}
-				</div>
-			@endif
-			@if(Auth::user() && Auth::user()->hasPermissionTo('add content') && $example->privateNotes)
-				<div class="field">
-					<span class="label">Private notes</span>
-					{!! replaceTags($example->privateNotes, $example->language->id) !!}
-				</div>
-			@endif
-		</div>
-	</div>
-	<span class="label">Sources</span>
-	@include('components.model.sourcelist', ['sources' => $example->sources])
+<div class="details">
+    {{-- Form --}}
+    @if($example->form)
+    <div class="detail-row">
+        <label class="detail-label">Form</label>
+        <div class="detail-value">
+            {!! $example->form->present('link') !!}
+        </div>
+    </div>
+    @endif
+
+    {{-- Phonology/Morphology --}}
+    <div class="detail-row">
+        <label class="detail-label">Morphology</label>
+        <div class="detail-value">
+            {!! $example->present('phonemicForm') !!}
+            {!! $example->present('morphemes') !!}
+        </div>
+    </div>
+
+    {{-- Translation --}}
+    <div class="detail-row">
+        <label class="detail-label">Translation</label>
+        <div class="detail-value">
+            {{ $example->translation }}
+        </div>
+    </div>
+
+    {{-- Parent --}}
+    @if($example->parent)
+    <div class="detail-row">
+        <label class="detail-label">Parent</label>
+        <div class="detail-value">
+            {{ $example->parent->present('link')->then('language')->as('link') }}
+        </div>
+    </div>
+    @endif
+
+    {{-- Public notes --}}
+    @isset($form->public_notes)
+    <div class="detail-row">
+        <label class="detail-label">Notes</label>
+        <div class="detail-value">
+            {!! replaceTags($form->public_notes, $form->language_id) !!}
+        </div>
+    </div>
+    @endisset
+
+    {{-- Private notes --}}
+    @if(Auth::user() && Auth::user()->hasPermissionTo('add content') && $example->private_notes)
+    <div class="detail-row">
+        <label class="detail-label">Private notes</label>
+        <div class="detail-value">
+            {!! replaceTags($form->private_notes, $form->language_id) !!}
+        </div>
+    </div>
+    @endif
+
+    {{-- Sources --}}
+    @if($example->sources->count() > 0)
+    <div class="detail-row">
+        <label class="detail-label">Sources</label>
+        <div class="detail-value">
+            @foreach($example->sources as $source)
+            <p>
+                {!! $source->present('link') !!}@isset($source->pivot->extra_info): {{ $source->pivot->extra_info }}@endisset
+            </p>
+            @endforeach
+        </div>
+    </div>
+    @endif
+</div>
 @endsection
